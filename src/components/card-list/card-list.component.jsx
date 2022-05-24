eeab92e1aed30e7874e1bf583aead92aa6f26b57
SAMY-MOHSEN-111 <11410120200220@stud.cu.edu.eg>
2022-05-24 02:48:35 +0200

    deleting the commented code

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -12,19 +12,3 @@ const CardList = (props) => {
   );
 };
 export default CardList;
-
-// import { Component } from "react";
-// import "./card-list.styles.css";
-// import Card from "../card/card.component";
-// class CardList extends Component {
-//   render() {
-//     return (
-//       <div className="card-list">
-//         {this.props.monsters.map((monster) => {
-//           return <Card monster={monster} key={monster.id} />;
-//         })}
-//       </div>
-//     );
-//   }
-// }
-// export default CardList;
